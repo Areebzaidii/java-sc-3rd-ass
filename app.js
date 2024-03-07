5c01239852d5bdba0cea20f82d0742766837c96d
areebzaidii <153609791+Areebzaidii@users.noreply.github.com>
2024-03-07 09:36:43 -0800

    okass

--- app.js
+++ app.js
@@ -338,7 +338,9 @@
 //     greeting = "Good evening";
 // }
 
-// QUESTION #07
-var time=+prompt("Enter time in 24 hours");
-if(time>=100){console.log("good morning");} 
-else if (time>150){console.log("good ")}
+// // QUESTION #07
+// var time = +prompt("Enter time in 24 hours");
+// if (time>="0000" && time<1200){console.log("good morning")}
+// else if(time>=1200 &&time<1700) {console.log("good afternoon")}
+// else if(time>=1700 &&time<2100) {console.log("good Evening")}
+// else if(time>=2100 &&time<=2359) {console.log("good night")}
